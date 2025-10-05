7f9bb4257d2f9268dc6615c501c37ff4ef11769a
SubelarioIsrael <israelsubelario123@gmail.com>
2025-10-05 21:27:43 +0800

    absolutely nothing

--- integration_test/login_test.dart
+++ integration_test/login_test.dart
@@ -8,15 +8,14 @@ void main() {
 
   testWidgets('User can log in', (tester) async {
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle();
+    await tester.pumpAndSettle(const Duration(seconds: 10));
 
     await tester.enterText(
-        find.byKey(const Key('login_email')), 'studenttestacc555@example.com');
-    await tester.enterText(
-        find.byKey(const Key('login_password')), 'testpassword123');
+        find.byKey(const Key('login_email')), 'boiblek04@gmail.com');
+    await tester.enterText(find.byKey(const Key('login_password')), 'lolz1433');
     await tester.tap(find.byKey(const Key('login_button')));
     await tester.pumpAndSettle(const Duration(seconds: 5));
 
     expect(find.text('Student Home'), findsOneWidget);
-  });
+  }, timeout: const Timeout(Duration(minutes: 5))); // Increase timeout);
 }
